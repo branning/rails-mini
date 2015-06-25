80ac1add579fbbb2a9bd0195b2a8e9b68e334ab4
Philip <branning@gmail.com>
2015-06-25 07:17:03 -0700

    add pagination to Users view

--- mini_proj/app/controllers/users_controller.rb
+++ mini_proj/app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all
+			.paginate(:page => params[:page], :per_page => params[:per])
+		@null_category = Category.none
 	end
 	def show
 		@user = User.find(params[:id])
 		@items = @user.items
 		# lambdas on rails, amirite
-		@recommended_items = @items.map {|item| item.categories}.flatten.uniq.map {|category| category.items }.flatten.group_by{|item| item}.map {|k, v| [k, v.length]}.sort {|a,b| b[1] <=> a[1]}
+		@recommended_items = @items
+			.map {|item| item.categories}.flatten.uniq
+			.map {|category| category.items }.flatten
+			.group_by{|item| item}
+			.map {|k, v| [k, v.length]}
+			.sort {|a,b| b[1] <=> a[1]}
 		# remove the items that were already purchased
 		@recommended_items.reject!{|pair| @items.include?(pair[0])}
 	end
